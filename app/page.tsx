5235f3c6af878bebe2c9ab402962e05b24f0bf22
Sara <sarailic431@gmail.com>
2026-04-10 21:24:02 +0200

    test authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  // Test
+  // Test authentication
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
