1cb13fd1b7b2debe4de4f26b2eb936c20fe73303
Sara <sarailic431@gmail.com>
2026-04-10 23:42:46 +0200

    login page, connecting firebase

---
+++ app/config/firebase-config.tsx
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+process.env.NEXT_PUBLIC_IME_VAR;
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_IME_VAR,
+  authDomain: "fir-course-c5a50.firebaseapp.com",
+  projectId: "fir-course-c5a50",
+  storageBucket: "fir-course-c5a50.firebasestorage.app",
+  messagingSenderId: "892770398416",
+  appId: "1:892770398416:web:ff955c918695d177512ed5",
+  measurementId: "G-B51KQ2W735",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
---
+++ app/login/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+
+const Login = () => {
+  const [inputEmail, setInputEmail] = useState<string>("");
+  const [inputPassword, setInputPassword] = useState<string>("");
+  return (
+    <form className="w-[400px] backdrop-blur-md bg-gradient-to-br from-gray-100/70 to-gray-300/60 shadow-xl rounded-2xl px-10 py-8 flex flex-col mx-auto items-center gap-5 mt-10 border border-gray-200">
+      <h1 className="text-gray-800 text-3xl self-start font-semibold">
+        Log In
+      </h1>
+
+      <input
+        type="text"
+        placeholder="Email"
+        className="w-full px-4 py-2 rounded-lg border border-gray-300 bg-white/80 focus:outline-none focus:ring-2 focus:ring-gray-400 transition"
+        value={inputEmail}
+        onChange={(e) => setInputEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        className="w-full px-4 py-2 rounded-lg border border-gray-300 bg-white/80 focus:outline-none focus:ring-2 focus:ring-gray-400 transition"
+        value={inputPassword}
+        onChange={(e) => setInputPassword(e.target.value)}
+      />
+
+      <button className="w-full py-2 rounded-lg bg-gray-800 text-white font-medium hover:bg-gray-700 transition duration-200 shadow-md">
+        Log In
+      </button>
+    </form>
+  );
+};
+
+export default Login;
