d5fed1403ab86928589cff7105a9aff6ce26f671
Sara <sarailic431@gmail.com>
2026-04-10 23:59:26 +0200

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 import { useState } from "react";
+import { auth } from "../config/firebase-config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
   const [inputEmail, setInputEmail] = useState<string>("");
   const [inputPassword, setInputPassword] = useState<string>("");
+
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, inputEmail, inputPassword);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="w-[400px] backdrop-blur-md bg-gradient-to-br from-gray-100/70 to-gray-300/60 shadow-xl rounded-2xl px-10 py-8 flex flex-col mx-auto items-center gap-5 mt-10 border border-gray-200">
+    <form
+      onSubmit={signIn}
+      className="w-[400px] backdrop-blur-md bg-gradient-to-br from-gray-100/70 to-gray-300/60 shadow-xl rounded-2xl px-10 py-8 flex flex-col mx-auto items-center gap-5 mt-10 border border-gray-200"
+    >
       <h1 className="text-gray-800 text-3xl self-start font-semibold">
         Log In
       </h1>
@@ -26,7 +40,10 @@ const Login = () => {
         onChange={(e) => setInputPassword(e.target.value)}
       />
 
-      <button className="w-full py-2 rounded-lg bg-gray-800 text-white font-medium hover:bg-gray-700 transition duration-200 shadow-md">
+      <button
+        type="submit"
+        className="w-full py-2 rounded-lg bg-gray-800 text-white font-medium hover:bg-gray-700 transition duration-200 shadow-md"
+      >
         Log In
       </button>
     </form>
